a991815dc66dd22b26d1e80f98bcda906614c447
Constantine Peresypkin <constantine@caspiandb.com>
2023-05-29 15:13:23 -0400

    add footer

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './userWorker';
 import { App } from "./components/App.tsx";
+import SqlFooter from "./components/Footer.tsx";
 
 ReactDOM.render(
     <React.StrictMode>
         <App/>
+        <SqlFooter/>
     </React.StrictMode>,
     document.getElementById('root')
 );
